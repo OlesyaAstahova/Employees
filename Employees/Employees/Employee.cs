ae105cb40e52359f40301a94b341e34316c2ef3b
Олесуа и Юля <astahova.98@gmail.com>
2018-02-06 12:43:08 +0700

    Управление созданием базового класса с помощью ключевого слова base

--- Employees/Employees/Employee.cs
+++ Employees/Employees/Employee.cs
@@ -111,10 +111,31 @@ namespace Employees
     }
     class Manager : Employee
     {
-        public int StockOptiona { get; set; }
+        public int StockOptions { get; set; }
+
+        public Manager(string fullName, int age, int empID, float currPay, string ssn, int numbOfOpts) : base (fullName, age, empID, currPay, ssn)
+        {
+            //Это свойство определено в классе Manager
+            StockOptions = numbOfOpts;
+            //Присвоить входные параметры, используя унаследованные свойства родительского класса
+            /*
+            ID = empID;
+            Age = age;
+            Name = fullName;
+            Pay = currPay;*/
+            //Ошибка компиляции,т.к. свойство доступно только для чтения
+           // SocialSecurityNumber = ssn;
+        }
+        public Manager() { }
     } 
+    // В качестве общего правила, все подклассы должны явно вызвать соответствующий конструктор базового класса
     class Salesperson : Employee
     {
         public int SalesNumber { get; set; }
+        public Salesperson(string fullName, int age, int empID, float currPay, string ssn, int numbOfSales) : base(fullName, age, empID, currPay, ssn)
+        {
+            SalesNumber = numbOfSales;
+        }
+        public Salesperson() { }
     }
 }
--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -16,6 +16,13 @@ namespace Employees
             fred.Age = 31;
             fred.Name = "Fred";
             fred.SalesNumber = 50;
+
+            fred.DisplayStats();
+            Console.WriteLine("\n***************\n");
+            //Предположим, что у Manager есть конструктор со следующей сигнатурой 
+            // (string fullName, int age, int empID, float currPay, string ssn, int numbOfOpts)
+            Manager chucky = new Manager("Chucky", 50, 92, 1000000, "333-23-2322", 9000);
+            chucky.DisplayStats();
             Console.ReadLine();
           
         }
