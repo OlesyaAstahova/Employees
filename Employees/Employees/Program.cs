711cb78fc4713170adc39d3bc0304b998601ea09
Олесуа и Юля <astahova.98@gmail.com>
2018-02-09 08:57:22 +0700

    Ключевое слово is

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -23,6 +23,16 @@ namespace Employees
         static void GivePromotion(Employee emp)
         {
             Console.WriteLine("{0} was promoted!", emp.Name);
+            if (emp is Salesperson)
+            {
+                Console.WriteLine("{0} made {1} sale(s)!", emp.Name, ((Salesperson)emp).SalesNumber);
+                Console.WriteLine();
+            }
+            if (emp is Manager)
+            {
+                Console.WriteLine("{0} had {1} stock options...!", emp.Name, ((Manager)emp).StockOptions);
+                Console.WriteLine();
+            }
         }
         static void Main(string[] args)
         {
